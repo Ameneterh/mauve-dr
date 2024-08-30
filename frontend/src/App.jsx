7e1b3bc9e810f7a835b9f48cfaf235f649415faf
amspharmacy <ameneterh@gmail.com>
2024-08-30 09:58:54 +0100

    built for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
-import viteLogo from "/vite.svg";
 import { Button } from "flowbite-react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HeaderComponent from "./Components/HeaderComponent";
 import HomePage from "./Pages/HomePage";
-// import "./App.css";
 
 function App() {
   return (
